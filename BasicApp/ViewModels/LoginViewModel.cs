a8aa132f73fce668772afdaf6696423a7e34233b
Marc Bruins <marc@marcbruins.nl>
2016-11-23 21:37:45 +0100

    initial commit

---
+++ BasicApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,47 @@
+using BasicApp.Core.Interfaces;
+using MvvmCross.Core.ViewModels;
+
+namespace BasicApp.Core.ViewModels
+{
+    public class LoginViewModel : MvxViewModel
+    {
+        private string _username;
+        public string Username
+        { 
+            get { return _username; }
+            set { SetProperty (ref _username, value); }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set { SetProperty(ref _password, value); }
+        }
+
+        private readonly ILoginService _loginService;
+
+        public LoginViewModel(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        private IMvxCommand _loginCommand;
+        public  IMvxCommand LoginCommand
+        {
+            get
+            {
+                _loginCommand = _loginCommand ?? new MvxCommand(AttemptLogin);
+                return _loginCommand;
+            }
+        }
+
+        private void AttemptLogin()
+        {
+            if (_loginService.Login(Username, Password))
+            {
+                ShowViewModel<MainViewModel>();
+            }
+        }
+    }
+}
---
+++ iOS/Views/LoginView.cs
@@ -0,0 +1,22 @@
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.iOS.Views;
+
+namespace BasicApp.iOS.Views
+{
+    public partial class LoginView : MvxViewController
+    {
+        public LoginView() : base("FirstView", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var set = this.CreateBindingSet<LoginView, Core.ViewModels.FirstViewModel>();
+            set.Bind(Label).To(vm => vm.Hello);
+            set.Bind(TextField).To(vm => vm.Hello);
+            set.Apply();
+        }
+    }
+}
